3a416c5a7123ae41dcbadd04fd5a66de2aff1bec
Cyril <estrelladacyril@gmail.com>
2026-02-11 23:41:14 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-black">
+      <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold text-center">
+        <span className="text-gray-300">CYRIL </span>
+        <span className="text-red-600">ESTRELLADA</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
