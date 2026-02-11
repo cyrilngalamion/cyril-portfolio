15329cc732f70654574dbe88753680869f9813ea
Cyril <estrelladacyril@gmail.com>
2026-02-11 23:59:14 +0800

    first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import profile from "./assets/wolfe.jpg";
+
 function App() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-black">
-      <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold text-center">
-        <span className="text-gray-300">CYRIL </span>
-        <span className="text-red-600">ESTRELLADA</span>
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black gap-6">
+      
+      {/* Image */}
+      <img
+        src={profile}
+        alt="Cyril"
+        className="w-250 h-150 object-cover border-4 border-white"
+      />
+
+      {/* Name */}
+      <h1 className="text-5xl md:text-7xl lg:text-8xl font-extrabold text-center">
+        <span className="text-gray-300">JOHNDEL </span>
+        <span className="text-red-600"> WOLFE</span>
       </h1>
+
     </div>
   );
 }
 
 export default App;
+
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/cyril-portfolio/",
   plugins: [
     react(), 
     tailwindcss(),
