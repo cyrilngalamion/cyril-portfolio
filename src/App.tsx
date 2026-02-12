af1f910ada392eb10211ded61127e9602ded649b
Cyril <estrelladacyril@gmail.com>
2026-02-12 10:57:54 +0800

    second update

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,33 @@ import profile from "./assets/wolfe.jpg";
 
 function App() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black gap-6">
-      
-      {/* Image */}
-      <img
-        src={profile}
-        alt="Cyril"
-        className="w-250 h-150 object-cover border-4 border-white"
-      />
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black gap-6 px-4">
+  
+  {/* Image */}
+  <img
+    src={profile}
+    alt="Cyril"
+    className="
+      w-full 
+      max-w-xs 
+      sm:max-w-sm 
+      md:max-w-md 
+      lg:max-w-lg 
+      h-auto 
+      object-cover 
+      border-4 
+      border-white
+    "
+  />
 
-      {/* Name */}
-      <h1 className="text-5xl md:text-7xl lg:text-8xl font-extrabold text-center">
-        <span className="text-gray-300">JOHNDEL </span>
-        <span className="text-red-600"> WOLFE</span>
-      </h1>
+  {/* Name */}
+  <h1 className="text-3xl sm:text-5xl md:text-7xl lg:text-8xl font-extrabold text-center leading-tight">
+    <span className="text-gray-300">JOHNDEL </span>
+    <span className="text-red-600">WOLFE</span>
+  </h1>
 
-    </div>
+</div>
+
   );
 }
 
